68e9572fac2c508430a69208d7698b0a4a0c278c
itwasrajesh-jpg <itwasrajesh@gmail.com>
2026-06-13 20:55:20 +0530

    Add files via upload

---
+++ app.config.js
@@ -0,0 +1,57 @@
+export default {
+  expo: {
+    name: "Celestial's Expense Tracker",
+    slug: "expense",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./icon.png",
+    userInterfaceStyle: "dark",
+    splash: {
+      image: "./splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#0E0C14"
+    },
+    assetBundlePatterns: ["**/*"],
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./adaptive-icon.png",
+        backgroundColor: "#0E0C14"
+      },
+      package: "com.celestial.expensetracker",
+      versionCode: 40,
+      permissions: [
+        "android.permission.VIBRATE",
+        "android.permission.USE_BIOMETRIC",
+        "android.permission.USE_FINGERPRINT",
+        "android.permission.READ_MEDIA_IMAGES"
+      ]
+    },
+    plugins: [
+      "expo-local-authentication",
+      "./withKotlinVersion",
+      [
+        "expo-build-properties",
+        {
+          android: {
+            kotlinVersion: "2.0.21",
+            compileSdkVersion: 36,
+            targetSdkVersion: 36,
+            minSdkVersion: 24,
+            buildToolsVersion: "36.0.0"
+          }
+        }
+      ],
+      [
+        "expo-speech-recognition",
+        {
+          "microphonePermission": "Allow Celestial to use your microphone for voice expense entry."
+        }
+      ]
+    ],
+    extra: {
+      eas: {
+        projectId: "4577b2c7-b2f3-4705-9450-4a9dc56b2910"
+      }
+    }
+  }
+};
